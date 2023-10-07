6c2707b55e003a7bd9f745adfb33168cbfb636d8
Christopher Bohlman <cbohlman@umd.edu>
2023-10-06 21:53:57 -0400

    exercise 3.8

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -5,7 +5,14 @@ const morgan = require("morgan");
 const MAX_ID = 1000000000000000;
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", (request, response) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
