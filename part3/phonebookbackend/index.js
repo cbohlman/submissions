ef0ed4c4f5fe4e2526f2b6d47efccfe354378b1c
Christopher Bohlman <cbohlman@umd.edu>
2023-10-08 21:26:38 -0400

    exercise 3.11

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -7,6 +7,7 @@ const MAX_ID = 1000000000000000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 
 morgan.token("body", (request, response) => {
   return JSON.stringify(request.body);
