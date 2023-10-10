07ec11757a3e2fabdc75430a5e25abc32cb1193e
Christopher Bohlman <cbohlman@umd.edu>
2023-10-09 20:35:36 -0400

    exercise 3.14

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 
-const MAX_ID = 1000000000000000;
+const Person = require("./models/person");
 
 app.use(cors());
 app.use(express.json());
@@ -17,33 +18,6 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const generateId = () => {
-  return Math.floor(Math.random() * MAX_ID);
-};
-
 app.get("/", (request, response) => {
   const content = `
         <p>Phonebook has info for ${persons.length} people</p>
@@ -54,43 +28,41 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((people) => {
+    response.json(people);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  if (!body.name) {
+  if (body.name === undefined) {
     return response.status(400).json({ error: "missing name" });
   }
-  if (!body.number) {
+  if (body.number === undefined) {
     return response.status(400).json({ error: "missing number" });
   }
-  if (
-    persons.filter(
-      (person) => person.name.toLowerCase() === body.name.toLowerCase()
-    ).length > 0
-  ) {
-    return response.status(400).json({ error: "Entry already exists" });
-  }
+  // if (
+  //   persons.filter(
+  //     (person) => person.name.toLowerCase() === body.name.toLowerCase()
+  //   ).length > 0
+  // ) {
+  //   return response.status(400).json({ error: "Entry already exists" });
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
