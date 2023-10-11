af7ba6473fd6a531284add1b5946678f9039ab30
Christopher Bohlman <cbohlman@umd.edu>
2023-10-11 18:58:41 -0400

    exercise 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -13,7 +13,7 @@ beforeEach(async () => {
   }
 });
 
-describe("GET Blogs", () => {
+describe("GET /blogs", () => {
   test("Notes are returned as JSON", async () => {
     await api
       .get("/api/blogs")
@@ -29,6 +29,28 @@ describe("GET Blogs", () => {
   test("Expect 'id' parameter to be defined", async () => {
     const response = await api.get("/api/blogs");
     expect(response.body[0].id).toBeDefined();
+  });
+});
+
+describe("POST /blogs", () => {
+  test("Valid blog can be added", async () => {
+    const newBlog = {
+      title: "test title",
+      author: "test author",
+      url: "test.com",
+      likes: 0,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    const contents = blogsAtEnd.map((b) => b.title);
+    expect(contents).toContain(newBlog.title);
   });
 });
 
