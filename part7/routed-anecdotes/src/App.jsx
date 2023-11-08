43fba3ca3b4439ccdb6bfb0a5086ff161f41f70b
Christopher Bohlman <cbohlman@umd.edu>
2023-11-07 22:58:54 -0500

    exercise 7.3

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Route, Routes, Link, useMatch } from "react-router-dom";
+import { Route, Routes, Link, useMatch, useNavigate } from "react-router-dom";
 
 const Menu = () => {
   const padding = {
@@ -123,6 +123,12 @@ const CreateNew = (props) => {
   );
 };
 
+const Notification = ({ notification }) => (
+  <div>
+    <p>{notification}</p>
+  </div>
+);
+
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
     {
@@ -142,10 +148,16 @@ const App = () => {
   ]);
 
   const [notification, setNotification] = useState("");
+  const navigate = useNavigate();
 
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000);
     setAnecdotes(anecdotes.concat(anecdote));
+    navigate("/");
+    setNotification(`Created ${anecdote.content}`);
+    setTimeout(() => {
+      setNotification("");
+    }, 5000);
   };
 
   const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
@@ -170,6 +182,7 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
+      <Notification notification={notification} />
       <Routes>
         <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
         <Route
