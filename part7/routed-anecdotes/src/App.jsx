2ee2a5cea39f48e3e025a3652aa8dcabfdb3f298
Christopher Bohlman <christophermbohlman@gmail.com>
2023-11-15 22:31:08 -0500

    exercise 7.5

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -93,6 +93,12 @@ const CreateNew = (props) => {
     });
   };
 
+  const handleReset = (e) => {
+    content.reset();
+    author.reset();
+    info.reset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -109,7 +115,10 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
       </form>
     </div>
   );
